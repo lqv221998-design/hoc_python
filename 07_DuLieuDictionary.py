7da72ce89e5697c89601efcfb495132ff70ae7a3
lqv221998-design <lqv221998@gmail.com>
2025-12-08 17:21:13 +0700

    Create 07_DuLieuDictionary.py

---
+++ 07_DuLieuDictionary.py
@@ -0,0 +1,12 @@
+# Cấu trúc dữ liệu Dictionary trong Python
+my_dict = {
+    "name": "Alice",          # Khóa 'name' với giá trị 'Alice'
+    "age": 30,                # Khóa 'age' với giá trị 30
+    "city": "New York"        # Khóa 'city' với giá trị 'New York'
+}
+print("Tên:", my_dict["name"])  # Truy cập giá trị bằng khóa 'name' Kết quả: Alice
+print("Tuổi:", my_dict["age"])   # Truy cập giá trị bằng khóa 'age' Kết quả: 30
+
+# Thêm một cặp khóa-giá trị mới
+my_dict["job"] = "Engineer"  # Thêm khóa 'job' với giá trị 'Engineer'
+print("Công việc:", my_dict["job"])  # Kết quả: Engineer
