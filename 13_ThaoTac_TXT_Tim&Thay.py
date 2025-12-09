518d515254f630e38f8ab2441e0c10d0f5cfb610
lqv221998-design <lqv221998@gmail.com>
2025-12-09 17:26:38 +0700

    Update 13_ThaoTac_TXT_Tim&Thay.py

--- 13_ThaoTac_TXT_Tim&Thay.py
+++ 13_ThaoTac_TXT_Tim&Thay.py
@@ -1,2 +1,7 @@
 # Tìm nội dung và thay thế trong file TXT
-# Đọc file 
+# Đọc file: Đưa toàn bộ nội dung vào danh sách rỗng
+danh_sach = "danh_sach_demo.txt"
+with open(danh_sach, "r", encoding="utf-8") as f:
+    content = f.read()
+    print(content)
+
