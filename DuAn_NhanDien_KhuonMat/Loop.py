7e0b200788e3948b35e3d8024f290586db3a1d8b
lqv221998-design <lqv221998@gmail.com>
2025-12-15 14:37:36 +0700

    Update Loop.py

--- DuAn_NhanDien_KhuonMat/Loop.py
+++ DuAn_NhanDien_KhuonMat/Loop.py
@@ -1,3 +1,9 @@
+#Cài đặt pip install CMake trước
+# cài pip install https://github.com/omwaman1/dlib/releases/download/dlib/dlib-19.24.99-cp313-cp313-win_amd64.whl
+# cài pip install --upgrade pip setuptools wheel
+#Cài đặt thư viện: pip install opencv-python
+#Cài đặt thư viện: pip install dlib
+
 my_list = [1, 2, 3]
 my_dict = {'a': 1, 'b': 2, 'c': 3}
 
